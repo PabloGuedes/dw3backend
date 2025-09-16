56edcc4113a47f7992dc15ba4cc1ae9e1b35b97c
PabloGuedes <guedespablobacca@gmail.com>
2025-09-16 07:50:31 -0300

    Pagina 65

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const routerApp = express.Router();
 
+const appLogin = require("../apps/login/controller/ctlLogin");
+
 // middleware that is specific to this router
 routerApp.use((req, res, next) => {
 next();
@@ -15,5 +17,7 @@ res.send("Olá mundo!");
 //@ Rotas de Cursos
 
 // Rota Login
+routerApp.post("/Login", appLogin.Login);
+routerApp.post("/Logout", appLogin.Logout);
 
 module.exports = routerApp;
